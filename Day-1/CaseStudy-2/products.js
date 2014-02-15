46d899fb477ca1d88b9aa7f553410d19ca524d3e
tkmagesh <tkmagesh77@gmail.com>
2014-02-14 16:45:31 -0800

    Case Study - 2 - Filtering Assignment

--- Day-1/CaseStudy-2/products.js
+++ Day-1/CaseStudy-2/products.js
@@ -11,8 +11,43 @@ function sort(list){
 	/*
 		implement the sort logic for sorting the list based on "id" (assuming that all the objects in the list have "id" attribute)
 	*/
+	for(var i=0;i<list.length-1;i++)
+		for(var j=i+1;j<list.length;j++){
+			if (list[i].id > list[j].id){
+				var temp = list[i];
+				list[i] = list[j];
+				list[j] = temp;
+			}
+		}
 }
 
 /*
 	Modify the above sort function in such way that the user can sort the list based on ANY attribute
 */
+
+function sort(list,comparerFn){
+	for(var i=0;i<list.length-1;i++)
+		for(var j=i+1;j<list.length;j++){
+			if (comparerFn(list[i],list[j]) > 0){
+				var temp = list[i];
+				list[i] = list[j];
+				list[j] = temp;
+			}
+		}
+}
+
+
+function sort(list,attrName){
+	for(var i=0;i<list.length-1;i++)
+		for(var j=i+1;j<list.length;j++){
+			if (list[i][attrName] > list[j][attrName]){
+				var temp = list[i];
+				list[i] = list[j];
+				list[j] = temp;
+			}
+		}
+}
+
+function filter(list,criteria){
+	//you decide what should be the implementation of criteria
+}
