149632f87a8ac58843fbdbf920957a9ab818c635
tkmagesh <tkmagesh77@gmail.com>
2014-02-15 17:05:47 -0800

    Day - 2 Complete

--- Day-2/Patterns/SalaryCalculator.js
+++ Day-2/Patterns/SalaryCalculator.js
@@ -3,13 +3,23 @@ function SalaryCalculator(){
 		this.hra = 0;
 		this.da = 0;
 		this.tax = 0;
+		this.subscribers = [];
 	}
 
 	SalaryCalculator.prototype.calculate = function(){
 		var gross = this.basic + this.hra + this.da;
 		var net = gross * ((100-this.tax)/100);
 		this.salary = net;
-		this.onSalaryChange();
+		this.triggerSalaryChange();
 	};
+
+	SalaryCalculator.prototype.addSubscriberForSalaryChange = function(callback){
+		this.subscribers.push(callback);
+	}
+
+	SalaryCalculator.prototype.triggerSalaryChange = function(){
+		for(var i=0;i<this.subscribers.length;i++)
+			this.subscribers[i]();
+	}
 
 	SalaryCalculator.prototype.onSalaryChange = function(){};
